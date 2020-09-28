b1a78e5b4b0df2eb9e31f8a5f6fd35d3d90830cb
conantroutman <johsod96@gmail.com>
2020-09-28 23:02:17 +0200

    Helicopter, plane and boat blips implemented

--- Client/HUD/Blips.cs
+++ Client/HUD/Blips.cs
@@ -1,6 +1,7 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
 using CitizenFX.Core.UI;
+using System;
 
 namespace Client.HUD
 {
@@ -9,17 +10,59 @@ namespace Client.HUD
         private static int blip;
         private static int vehicle;
         private static PlayerList players;
+        private static bool isBlipRotating = false;
+        private enum VehicleBlips
+        {
+            
+        }
 
         public async static void Update()
         {
             foreach(Player player in players)
             {
-                blip = API.GetBlipFromEntity(player.Character.Handle);
+                blip = player.Character.AttachedBlip.Handle;
                 if(player.Character.IsDead)
                 {
-                    API.SetBlipSprite(blip, (int)BlipSprite.Dead);
+                    SetBlip(player, blip, (int)BlipSprite.Dead, false, false);
+                } else
+                {
+                    if (player.Character.IsInVehicle())
+                    {
+                        switch(player.Character.CurrentVehicle.ClassType)
+                        {
+                            case VehicleClass.Helicopters:
+                                SetBlip(player, blip, (int)BlipSprite.HelicopterAnimated, false, false);
+                                break;
+                            case VehicleClass.Planes:
+                                SetBlip(player, blip, (int)BlipSprite.Plane, false, true);
+                                break;
+                            case VehicleClass.Boats:
+                                SetBlip(player, blip, 427, false, true);
+                                break;
+                        }
+                    } else
+                    {
+                        SetBlip(player, blip, 1, true, false);
+                    }
+                }
+
+                if (isBlipRotating)
+                {
+                    API.SetBlipRotation(blip, (int)Math.Ceiling(API.GetEntityHeading(player.Character.Handle)));
                 }
             }
+        }
+
+        private static void SetBlip(Player player, int blip, int sprite, bool showHeading, bool isRotating)
+        {
+            if (API.GetBlipSprite(blip) != sprite)
+            {
+                API.SetBlipSprite(blip, sprite);
+                API.SetBlipColour(blip, Players.Colors.GetColor(Game.Player) - 22);
+                API.ShowHeadingIndicatorOnBlip(blip, showHeading);
+                API.SetBlipNameToPlayerName(blip, player.Handle);
+            }
+            isBlipRotating = isRotating;
         }
 
         public static void Create()
@@ -30,10 +73,7 @@ namespace Client.HUD
                 if (!API.DoesBlipExist(blip))
                 {
                     blip = API.AddBlipForEntity(player.Character.Handle);
-                    API.SetBlipSprite(blip, 1);
-                    API.ShowHeadingIndicatorOnBlip(blip, true);
-                    API.SetBlipColour(blip, Players.Colors.GetColor(Game.Player) - 22);
-                    API.SetBlipNameToPlayerName(blip, player.Handle);
+                    SetBlip(player, blip, 1, true, false);
                     API.SetBlipCategory(blip, 7);
                 }
             }
